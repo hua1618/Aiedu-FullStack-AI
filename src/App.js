e15bc9189f8d744c1df16c75c5298b60585b6dab
Anita <aneeta.ashraf7@gmail.com>
2022-11-09 10:35:17 +0200

    move the files to the parent directory

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button className='bookingbutton'>Book Course</button>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
