b8b2fa4cce1a05c7aa862189710cb1e5532b9390
Hanh Duong <dg.hanh117@gmail.com>
2022-11-09 13:24:26 +0200

    test new bookingform branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button className='bookingbutton'>Book Course</button>
+        <button className='bookingbutton'>Book</button>
         
       </header>
     </div>
