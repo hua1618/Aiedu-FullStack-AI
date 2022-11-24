4be04b3ba7fb74912906ce8008bd6eee5595140d
Anita <aneeta.ashraf7@gmail.com>
2022-11-24 15:47:53 +0200

    added extentions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
-//import Home from './components/Home.jsx';
-import RegisterForm from './components/RegisterForm';
-import StudentsProjects from './components/StudentsProjects';
-import Login from './components/Login';
+import Home from './components/Home.jsx';
+import RegisterForm from './components/RegisterForm.jsx';
+import StudentsProjects from './components/StudentsProjects.jsx';
+import Login from './components/Login.jsx';
 
 
 
@@ -12,7 +12,7 @@ export default function App(){
   return (
     <BrowserRouter>
     <Routes>
-      {/*<Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<Home />} /> 
       <Route path="/RegisterForm" element={<RegisterForm />} />
       <Route path="/StudentsProjects" element={<StudentsProjects/>} />
       <Route path="/Login" element={<Login />} />
