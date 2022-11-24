6c353ee7864d06d274fe77ff13d32f53e1cee4f8
Anita <aneeta.ashraf7@gmail.com>
2022-11-24 15:43:05 +0200

    comment out Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
-import Home from './components/Home';
+//import Home from './components/Home.jsx';
 import RegisterForm from './components/RegisterForm';
 import StudentsProjects from './components/StudentsProjects';
 import Login from './components/Login';
@@ -12,7 +12,7 @@ export default function App(){
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/*<Route path="/" element={<Home />} /> */}
       <Route path="/RegisterForm" element={<RegisterForm />} />
       <Route path="/StudentsProjects" element={<StudentsProjects/>} />
       <Route path="/Login" element={<Login />} />
