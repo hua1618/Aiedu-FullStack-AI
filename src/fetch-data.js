8bd9876ea647fae98dcc34d8d201273260baaf8a
Hanh Duong <dg.hanh117@gmail.com>
2022-12-05 14:41:17 +0200

    add mutation query

--- src/fetch-data.js
+++ src/fetch-data.js
@@ -19,18 +19,46 @@ query lookupCustomerOrder {
     description5
     description6
   }
-}
-
-
-
-
-  
+} 
 `;
+
 export const fetchCourseData = async () => {
   const response = await fetch(ENDPOINT_URL, {
     method: "POST",
     body: JSON.stringify({
       query: COMMENTS_QUERY
+    })
+  });
+  return await response.json();
+};
+
+
+const REGISTRATION_INPUT = `
+mutation InsertUserData($whatsApp: Boolean, $codingSkills: String, $comments: String, $email: String, $phone: String, $studentAge: String, $studentGender: String, $studentName: String, $teachingLanguage: String, $teachingMethod: String, $yourName: String) {
+  insert_UserData(objects: {whatsApp: $whatsApp, codingSkills: $codingSkills, comments: $comments, email: $email, phone: $phone, studentAge: $studentAge, studentGender: $studentGender, studentName: $studentName, teachingLanguage: $teachingLanguage, teachingMethod: $teachingMethod, yourName: $yourName}) {
+    affected_rows
+    returning {
+      whatsApp
+			codingSkills
+			comments
+			email
+			phone
+			studentAge
+			studentGender
+			studentName
+			teachingLanguage
+			teachingMethod
+			yourName
+    }
+  }
+}
+`
+export const submitRegistrationData = async (variables) => {
+  const response = await fetch(ENDPOINT_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      query: REGISTRATION_INPUT,
+      variables
     })
   });
   return await response.json();
